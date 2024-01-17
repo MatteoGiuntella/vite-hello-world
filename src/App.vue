4c6bc5d3ddaa3b6a5382cc5c7f191f342baf3dc3
MatteoGiuntella <145116327+MatteoGiuntella@users.noreply.github.com>
2024-01-17 14:50:24 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+  // my-component.js
+export default {
+    data() {
+       return { 
+        count: 0 
+      }
+    },
+    components:{
+      AppHeader
+    }
+}
+</script>
+
+<template>
+  <div>
+  <AppHeader></AppHeader>
+    <h1>maIN</h1>
+  </div> 
+</template>
+
+<style scoped>
+  h1{
+
+    color: red;
+  }
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+  // my-component.js
+export default {
+    data() {
+       return { 
+        title : 'bella li' 
+      }
+    },
+}
+</script>
+
+<template>
+  <h3>
+    {{ title }}
+  </h3>
+</template>
+
+<style scoped>
+  h3{
+    color: blue;
+  }
+</style>
